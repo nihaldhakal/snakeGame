41344113f1922558f505b3883faa3a3f5e5f6150
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-04-30 10:03:56 +0545

    Kept random value to the width of svg

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 setInterval(function(){
     var rectangle = document.getElementById("pixel");
-    var  x =  10* Math.random();
-    var  y =  10* Math.random();
-    rectangle.innerHTML= "<rect parseInt(x)=" +  x  +   "parseInt(y)=" +  y  +  " width=10 height=10 style='stroke:black;stroke-width:'/>";
+    var  x =  540* Math.random();
+    var  y =  350* Math.random();
+    rectangle.innerHTML= "<rect x=" +  parseInt(x)  +   " y=" +  parseInt(y)  +  " width=10 height=10 style='stroke:black;stroke-width:'/>";
     console.log(rectangle,parseInt(x),parseInt(y));
 },1000);
 
